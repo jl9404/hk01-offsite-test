2376cc8f9afaff022fe77e84d36e9a278ae3c696
Jason Leung <jl9404@hotmail.com>
2017-06-08 00:23:01 +0800

    tab space

--- app/Services/Payment/Providers/PaymentServiceProvider.php
+++ app/Services/Payment/Providers/PaymentServiceProvider.php
@@ -8,17 +8,17 @@ use Illuminate\Support\ServiceProvider;
 
 class PaymentServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
+    public function boot()
+    {
         Validator::extend('ccname', 'App\Services\Payment\Validators\CreditCardValidator@validateName');
         Validator::extend('currency', 'App\Services\Payment\Validators\CreditCardValidator@validateCurrency');
         Validator::extend('ccnumber', 'App\Services\Payment\Validators\CreditCardValidator@validateNumber');
         Validator::extend('ccdate', 'App\Services\Payment\Validators\CreditCardValidator@validateExpDate');
         Validator::extend('cvv', 'App\Services\Payment\Validators\CreditCardValidator@validateCvv');
-	}
+    }
 
-	public function register()
-	{
-		$this->app->singleton(Gateway::class);
-	}
+    public function register()
+    {
+        $this->app->singleton(Gateway::class);
+    }
 }
--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -11,44 +11,44 @@ use Carbon\Carbon;
 
 class PaymentService
 {
-	protected $gatewayFactory;
-	protected $gateway;
-	protected $transactionId;
+    protected $gatewayFactory;
+    protected $gateway;
+    protected $transactionId;
 
-	public function __construct(GatewayFactory $gateway)
-	{
-		$this->gatewayFactory = $gateway;
-	}
+    public function __construct(GatewayFactory $gateway)
+    {
+        $this->gatewayFactory = $gateway;
+    }
 
-	public function generateTransactionId()
-	{
-		return $this->transactionId = Carbon::now()->format('YmdHis') . random_int(1000, 9999);
-	}
+    public function generateTransactionId()
+    {
+        return $this->transactionId = Carbon::now()->format('YmdHis') . random_int(1000, 9999);
+    }
 
-	public function getGateway($currency)
-	{
-		if (in_array($currency, ['USD', 'EUR', 'AUD'])) { // amex usd validation is done in formrequest
+    public function getGateway($currency)
+    {
+        if (in_array($currency, ['USD', 'EUR', 'AUD'])) { // amex usd validation is done in formrequest
             return $this->gateway = $this->gatewayFactory->driver('paypal');
         } else if (in_array($currency, ['HKD', 'JPY', 'CNY'])) {
             return $this->gateway = $this->gatewayFactory->driver('braintree');
         }
         return null;
-	}
+    }
 
-	public function makePayment(array $attributes = [])
-	{
-		return $this->gateway->purchase(array_merge($attributes, ['transaction_id' => $this->transactionId]));
-	}
+    public function makePayment(array $attributes = [])
+    {
+        return $this->gateway->purchase(array_merge($attributes, ['transaction_id' => $this->transactionId]));
+    }
 
-	public function saveRecord(array $attributes = [], ResponseContract $response)
-	{
-		$transaction = new Transaction($attributes);
+    public function saveRecord(array $attributes = [], ResponseContract $response)
+    {
+        $transaction = new Transaction($attributes);
         $transaction->transaction_id = $this->transactionId;
         $transaction->reference_id = $response->getReferenceId();
         $transaction->paid_at = $response->getPaidTimestamp();
         $transaction->debug = serialize($response->getDebugData());
         $transaction->save();
         return $transaction;
-	}
+    }
 
 }
